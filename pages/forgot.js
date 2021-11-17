612959fc3b2c19d539e31d8256e19b708bd8c2d3
shoaibbadshah <shoaibbadshah45@gmail.com>
2021-11-17 09:37:13 +0500

    placeholder back link

--- pages/forgot.js
+++ pages/forgot.js
@@ -1,25 +1,27 @@
+import Link from "next/link";
+
 export default function forgot() {
   return (
     <div>
       <div className="flex flex-col items-center justify-center w-screen h-screen bg-gray-200 text-gray-700">
         <form
-          className="flex flex-col bg-white rounded shadow-lg p-12 mt-12"
+          className="flex flex-col bg-white rounded shadow-lg p-16 mt-12"
           action=""
         >
-          <label className="font-semibold text-xs" htmlFor="usernameField">
-            Email
-          </label>
           <input
-            className="flex items-center h-12 px-4 w-64 bg-gray-200 mt-2 rounded focus:outline-none focus:ring-2"
+            className="flex items-center h-12 px-4 w-64 border mt-2 rounded focus:outline-none focus:ring-2"
+            placeholder="Enter Email"
             type="text"
           />
 
-          <button className="flex items-center justify-center h-12 px-6 w-64 bg-blue-600 mt-8 rounded font-semibold text-sm text-blue-100 hover:bg-blue-700">
+          <button className="flex items-center justify-center h-12 px-6 w-64 bg-purple-600 mt-8 rounded font-semibold text-sm text-purple-100 hover:bg-purple-700">
             Confirm
           </button>
-          <button className="flex items-center justify-center h-12 px-6 w-64 bg-gray-200 mt-3 rounded font-semibold text-sm text-white-100 hover:bg-gray-300">
-            Go Back
-          </button>
+          <Link href="/">
+            <button className="flex items-center justify-center h-12 px-6 w-64 bg-gray-200 mt-3 rounded font-semibold text-sm text-white-100 hover:bg-gray-300">
+              Go Back
+            </button>
+          </Link>
         </form>
       </div>
     </div>
